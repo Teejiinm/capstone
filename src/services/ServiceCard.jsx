c2b3b9e4759dfbc754f30f7ff09c54222359ea71
anu <anudariin36@gmail.com>
2025-05-20 00:22:52 +0900

    commitabout

--- src/services/ServiceCard.jsx
+++ src/services/ServiceCard.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import { FaCloudSun, FaSuitcase, FaArrowRight } from 'react-icons/fa';
-import './service-card.css';
 import { GiDumplingBao, GiHorseHead } from 'react-icons/gi';
 import { Link } from 'react-router-dom';
+import './service-card.css';
 
 const cardData = [
-  { id: 1, icon: <FaCloudSun />, title: 'Weather & Geography' },
-  { id: 2, icon: <GiDumplingBao />, title: 'Food & Drinks' },
-  { id: 3, icon: <FaSuitcase />, title: 'Packing List' },
-  { id: 4, icon: <GiHorseHead />, title: 'To-Do List' },
+  {
+    id: 1,
+    icon: <FaCloudSun />,
+    title: 'Weather & Geography',
+    path: '/about',
+    hash: '#weather',
+  },
+  {
+    id: 2,
+    icon: <GiDumplingBao />,
+    title: 'Food & Drinks',
+    path: '/about',
+    hash: '#food',
+  },
+  {
+    id: 3,
+    icon: <FaSuitcase />,
+    title: 'Packing List',
+    path: '/about',
+    hash: '#packing',
+  },
+  {
+    id: 4,
+    icon: <GiHorseHead />,
+    title: 'To-Do List',
+    path: '/about',
+    hash: '#todo',
+  },
 ];
 
 const ServiceCard = () => {
@@ -19,27 +43,27 @@ const ServiceCard = () => {
     <section className="info__section">
       <Container>
         <Row className="justify-content-center gy-4">
-  {cardData.map((card) => (
-  <Col lg="6" md="6" key={card.id}>
-    <Link to="/about" className="info__card__link">
-
-      <div
-        className={`info__card ${hoveredCard === card.id ? 'hovered' : ''}`}
-        onMouseEnter={() => setHoveredCard(card.id)}
-        onMouseLeave={() => setHoveredCard(null)}
-      >
-        <div className="info__icon">{card.icon}</div>
-        <h5 className="info__title">{card.title}</h5>
-        <button className="info__go">
-          <FaArrowRight />
-        </button>
-      </div>
-    </Link>
-  </Col>
-))}
-
-</Row>
-
+          {cardData.map((card) => (
+            <Col lg="6" md="6" key={card.id}>
+              <Link
+                to={{ pathname: card.path, hash: card.hash }}
+                className="info__card__link"
+              >
+                <div
+                  className={`info__card ${hoveredCard === card.id ? 'hovered' : ''}`}
+                  onMouseEnter={() => setHoveredCard(card.id)}
+                  onMouseLeave={() => setHoveredCard(null)}
+                >
+                  <div className="info__icon">{card.icon}</div>
+                  <h5 className="info__title">{card.title}</h5>
+                  <button className="info__go" aria-label={`Go to ${card.title}`}>
+                    <FaArrowRight />
+                  </button>
+                </div>
+              </Link>
+            </Col>
+          ))}
+        </Row>
       </Container>
     </section>
   );
